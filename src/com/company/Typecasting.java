0a512c65cf84c622e2cdf4a2f9d7e99695c3d0a0
anusha <anuskasthacr7@gmail.com>
2022-02-06 20:11:15 +0545

    typecasting

---
+++ src/com/company/Typecasting.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Typecasting {
+
+    public static void main(String[] args) {
+        int breadth = 20;
+        int length = 10;
+
+        double Area = ((0.5) * length * breadth);
+        int area = (int) Area;
+
+
+        System.out.println("Area of triangle=" + area);
+    }
+}
+
